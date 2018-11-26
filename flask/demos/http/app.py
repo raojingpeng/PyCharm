ce6bf076c763878bc7d7dd69d7d0c6f7053e683a
raojingpeng <jingpeng.rao@gmail.com>
2018-11-26 13:00:17 +0800

    add app

--- flask/demos/http/app.py
+++ flask/demos/http/app.py
@@ -3,3 +3,18 @@
 # @Time    : 2018/11/26 11:34
 # @Author  : raojingpeng
 # @File    : app.py.py
+
+
+from flask import Flask, request, redirect, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return '<h1>Hello %s!</h1>' % name
